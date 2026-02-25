d4be12b87bd18988cc91ccafa976bf9538885360
Gilles DESBONNETS <gillesdesbonnets@gmail.com>
2026-02-25 18:59:02 +0100

    feat(components) ajout du profil

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Navbar from './components/navbar/Navbar'
+import Profile from './components/profile/Profile'
 
 function App() {
   return (
     <div className="app">
       <Navbar />
       <main>
+        <Profile />
       </main>
       <footer className="footer">
         <div className="container">
